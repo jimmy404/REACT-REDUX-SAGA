a48ac6145a9009f3cd536af1edcded71c8a8cd33
Jimmy <bruno_pier@hotmail.com>
2020-02-17 19:18:41 -0300

    Adding buttons&code - src/pages/Home/index.js

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,37 @@
-import React from 'react';
-import {Container, Typography} from '@material-ui/core';
+import React, {useState} from 'react';
+import {Container, Typography, Card, Grid, TextField, Button} from '@material-ui/core';
 
 export default () => {
+
+    const [searchText, setSearchText] = useState('');
+    const handleSearchTextChange = event => {
+        setSearchText(event.target.value);
+    };
+
+    const handleCleanTextClick = event => {console.log(10)};
+    const handleSearchTextClick = event => {console.log(10)};
+
     return(
         <Container>
-            Home
+            <Card>
+                <Grid container>
+                    <Grid>
+                        <Typography>Bienvenido</Typography>
+                    </Grid>
+                    <Grid>
+                        <label>Icono</label>
+                    </Grid>
+                </Grid>
+                <TextField
+                    value={searchText}
+                    placeholder="Buscar..."
+                    onChange={handleSearchTextChange}
+                />
+                <Grid>
+                    <Button variant="contained" onClick={handleCleanTextClick}>Limpiar</Button>
+                    <Button variant="contained" color="primary" size="large" onClick={handleSearchTextClick}>Buscar</Button>
+                </Grid>
+            </Card>
         </Container>
     )
 }
