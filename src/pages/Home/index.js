88ab16026d38a100083eef662dad69a27c61b9d0
Jimmy <bruno_pier@hotmail.com>
2020-02-17 18:24:35 -0300

    Adding return - src/pages/Home/index.js

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,10 @@
 import React from 'react';
 import {Container, Typography} from '@material-ui/core';
 
-export default () => (<Container><Typography variant="h1">Home</Typography></Container>)
+export default () => {
+    return(
+        <Container>
+            Home
+        </Container>
+    )
+}
