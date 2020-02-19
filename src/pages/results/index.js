461c5f895ff48606a01cf93065ce58f1394fb95d
Jimmy <bruno_pier@hotmail.com>
2020-02-19 00:34:05 -0300

    Adding useDispatch&useSelector code - src/pages/results/index.js

--- src/pages/results/index.js
+++ src/pages/results/index.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
 import {Container} from '@material-ui/core';
 import queryString from 'query-string';
 
+import {searchMovie} from '../../redux/actions/search';
+
+
 export default ({location}) => {
-    console.log(queryString.parse(location.search))
+
+    const dispatch = useDispatch();
+    const movies = useSelector(state =>{
+        console.log(state);
+    });
+
+    useEffect(() => {
+        const {movieName} = queryString.parse(location.search);
+        dispatch(searchMovie({movieName}));
+    });
+
+
     return(
         <Container>
             Results
