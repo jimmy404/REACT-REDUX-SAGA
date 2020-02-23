d03a870cfad17518392f51bbf745dcb7f13b3323
Jimmy <bruno_pier@hotmail.com>
2020-02-23 14:33:43 -0300

    Adding new cases - src/redux/reducers/search.js

--- src/redux/reducers/search.js
+++ src/redux/reducers/search.js
@@ -1,5 +1,11 @@
 import {get} from 'lodash';
-import {SEARCH_MOVIE_START, SEARCH_MOVIE_ERROR, SEARCH_MOVIE_COMPLETE} from '../../consts/actionTypes';
+import {SEARCH_MOVIE_START,
+    SEARCH_MOVIE_ERROR,
+    SEARCH_MOVIE_COMPLETE,
+    SEARCH_MOVIE_BY_ID_START,
+    SEARCH_MOVIE_BY_ID_ERROR,
+    SEARCH_MOVIE_BY_ID_COMPLETE
+} from '../../consts/actionTypes';
 
 const initialState = {};
 
@@ -9,10 +15,20 @@ export default function(state = initialState, action) {
             return {...state, isLoading: true};
             break;
         case SEARCH_MOVIE_ERROR:
-            return {...state, isLoading: false, movies: null};
+            return {...state, isLoading: false, movieResults: null};
             break;
         case SEARCH_MOVIE_COMPLETE:
             return {...state, isLoading: false, movieResults: action.results.data};
+            break;
+        case SEARCH_MOVIE_BY_ID_START:
+            return {...state, isLoading: true, movieResult: null};
+            break;
+        case SEARCH_MOVIE_BY_ID_ERROR:
+            return {...state, isLoading: false, movieResult: null};
+            break;
+        case SEARCH_MOVIE_BY_ID_COMPLETE:
+            console.log(action)
+            return {...state, isLoading: false, movieResult: action.movie.data};
             break;
         default:
             return {...state};
