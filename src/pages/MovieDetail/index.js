e69526ecb1d5d2e464885eedf7861143f9f22579
Jimmy <bruno_pier@hotmail.com>
2020-02-23 14:15:05 -0300

    Adding new folder&file - src/pages/MovieDetail/

---
+++ src/pages/MovieDetail/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Container} from '@material-ui/core';
+
+export default () => {
+    return(
+        <Container>
+            MovieDetail
+        </Container>
+    )
+}
